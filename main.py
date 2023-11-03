e1f69686ed16961301cea1871896222118a4b444
Konstantin Wohllaub <konstantin.wohllaub@gmail.com>
2023-11-03 15:49:50 +0100

    Hinzufügen von Beschreibungen für Visualisierungen

--- main.py
+++ main.py
@@ -125,36 +125,70 @@ https://figshare.com/articles/figure/DVM-CAR_Dataset/19586296/2""")
     - Getriebetyp (Gearbox)
     - Kraftstoffart (Fuel type)
     - Farbe (Color)""")
+        st.subheader("Explorative Datenanalyse")
+        st.write("""Im folgenden werden einige Visualisierungen eingeblendet, die dem Lesenden ein besseres Verständnis für den Datensatz geben soll.""")
         st.subheader("Prozentualer Anteil fehlender Werte")
         image1 = Image.open('Visualizations/missing_values.png')
-        st.image(image1, width=900)
+        st.image(image1, width=600)
+        st.write("""Diese Grafik bietet einen Überblick über die Qualität unseres Datensatzes, indem sie den prozentualen Anteil fehlender Werte in verschiedenen 
+                 Attributen darstellt. Ein hoher Prozentsatz von fehlenden Werten in einem Attribut kann auf Datenqualitätsprobleme hinweisen und beeinflusst die 
+                 Analyse und Modellierung. In der Grafik ist deutlich zu sehen, dass die meisten Null-Values in den Attributen Annual_Tax, Top_Speed und Average_mpg
+                 auftreten. Mögliche Lösungsansätze sind: \n 1. Löschen der jeweiligen Spalte \n 2. Ersetzen der Null-Values durch z.B. den Durchschnitt oder Median
+                 Spalte""")
         st.subheader("Korrelations-Heatmap")
         image2 = Image.open('Visualizations/Korr_heatmap.png')
-        st.image(image2, width=900)
+        st.image(image2, width=600)
+        st.write("""Diese beeindruckende Heatmap bietet einen visuellen Einblick in die Korrelation zwischen den numerischen Attributen unseres Datensatzes. Die 
+                 Heatmap ist ein leistungsstarkes Werkzeug, um Muster und Beziehungen zwischen verschiedenen Merkmalen zu erkennen. Zu beachten ist allerdings, dass
+                 diese Heatmap auf den unbearbeiteten Datensatz angewendet wurde, wodurch manche numerische Spalten fehlen, da sie in dieser Form des Datensatzes noch
+                 als Objekte codiert waren. Dennoch kann sehr gut die Stärke der linearen Beziehungen zwischen Preis und den restlichen Spalten beobachtet werden. Bei
+                 den zugrundeliegenden Datensatz ist die lineare Beziehung zwischen Preis und Motorleistung am stärksten.""")
         st.subheader("Anzahl Autos pro Hersteller")
         image3 = Image.open('Visualizations/anzahl_autos.png')
-        st.image(image3, width=900)
+        st.image(image3, width=750)
+        st.write("""Diese Balkendiagramm-Visualisierung bietet einen Überblick über die Anzahl der Autos, die von verschiedenen Herstellern in unserem Datensatz
+                 vertreten sind. Dies ermöglicht es, die Verteilung der Automarken im Datensatz zu verstehen. Die Hersteller die die meisten Autos in unseren
+                 Datensatz haben, sind Ford, Audi, Vauxhall, Volkswagen und BMW""")
         st.subheader("Verteilungen aller numerischen Datensatz-Attribute")
         image4 = Image.open('Visualizations/num_verteilung.png')
         st.image(image4, width=900)
+        st.write("""Diese Visualisierung zeigt die Verteilung der Werte in unseren numerischen Attributen. Die Darstellung erfolgt in einem Raster von Histogrammen,
+                 wobei jedes Histogramm ein anderes numerisches Attribut darstellt. Besonders aufällig ist die Verteilung des Preis-Attributs. Da es in dem Datensatz
+                 extrem teure Autos gibt, wird diese Verteilung als ein einzelner Strich dargestellt.""")
         st.subheader("Verteilungen aller kategorischen Datensatz-Attribute")
         image5 = Image.open('Visualizations/cat_verteilung.png')
-        st.image(image5, width=900)
+        st.image(image5, width=750)
+        st.write("""Diese Visualisierung zeigt die Verteilung der Werte in unseren kategorischen Attributen. Die Darstellung erfolgt in einem Raster von Histogrammen,
+         wobei jedes Histogramm ein anderes kategorisches Attribut darstellt.""")
         st.subheader("Durchschnittlicher Preis je Hersteller")
         image6 = Image.open('Visualizations/mean_price.png')
-        st.image(image6, width=900)
+        st.image(image6, width=800)
+        st.write("""Diese Balkendiagramm-Visualisierung zeigt den Durchschnittspreis der Autos für ausgewählte Hersteller in unserem Datensatz. Sie bietet Einblicke 
+                 in die Preisgestaltung unterschiedlicher Hersteller und kann bei der Analyse von Markenpräferenzen und Preisstrukturen nützlich sein. Zu sehen ist,
+                 dass Lamborghini, McLaren, Ferrari und Rolls-Royce die mit Abstand teuersten Autos im Datensatz besitzen. Allerdings wurden in der Darstellung
+                 nur Autos unter einem Preis von 1 Million $ berücksichtigt, da sonst die Ansehnlichkeit der Visualisierung darunter gelitten hätte.""")
         st.subheader("Durchschnittlicher Preis über Zeit")
         image7 = Image.open('Visualizations/price_over_time.png')
-        st.image(image7, width=900)
+        st.image(image7, width=700)
+        st.write("""Diese Liniendiagramm-Visualisierung veranschaulicht die Entwicklung des durchschnittlichen Auto-Preises im Laufe der Zeit für ausgewählte 
+                 Hersteller. Sie ermöglicht es, Trends in der Preisgestaltung verschiedener Hersteller im Zeitverlauf zu identifizieren.""")
         st.subheader("Scatterplot von Laufleistung und Preis")
         image8 = Image.open('Visualizations/miles_price_scatter.png')
-        st.image(image8, width=900)
-
+        st.image(image8, width=700)
+        st.write("""Diese Streudiagramm-Visualisierung zeigt das Verhältnis zwischen der Laufleistung und dem Preis von Autos für ausgewählte Hersteller. Sie hilft
+                 dabei, mögliche Zusammenhänge zwischen diesen beiden Faktoren zu erkennen und ihre Auswirkungen auf den Fahrzeugmarkt zu verstehen.""")
+        
     if (selected == "Vorhersage des Auto Preis"):
         st.title("Vorhersage des Auto Preis")
         st.text("Bitte geben Sie in die unteren Eingabefelder die Daten ihres Fahrzeuges ein.")
         st.text("Beachten Sie dabei die Groß- und Kleinschreibung und dass die Sprache der Eingabewerte Englisch ist.")
-        
+        st.subheader("R2-Scores der verschiedenen Modelle")
+        st.write("""Lineare Regression: -1.0700188322042872e+16 
+                 \n Lasso Regression: 0.5988089083316444 
+                 \n Random Forest Regressor: 0.861095177381202 
+                 \n Gradient Boosting Regressor 0.8141688072472406 
+                 \n AdaBoost Regressor: 0.61401889818197""")
+        st.subheader("Vorhersage")
         prediction()
 
 if __name__ == "__main__":
